005b2201187b5e6ab69e2d3259e2d93ae6af8fe7
Loskutova Marina <mloskutova1589@gmail.com>
2024-08-07 17:21:35 +0300

    first commit

---
+++ PlanetCatalog.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Program2
+{
+    public class PlanetCatalog
+    {
+        private readonly List<Planet> _planets = new();
+        private int _serialNumberFromTheSun;
+        private int _equatorLength;
+        private string? _errorMessage;
+        private Planet? _head;
+        private int _counter = 1;
+
+        public PlanetCatalog(params Planet[] inputs)
+        {
+            var listOfInputs = new List<Planet>();
+            foreach (var input in inputs)
+            {
+                var newHead = new Planet(input.Name, input.SerialNumberFromTheSun, input.EquatorLength);
+                newHead.PreviousPlanet = _head;
+                _head = newHead;
+                listOfInputs.Add(newHead);
+            }
+            this._planets = listOfInputs;
+        }
+
+        public (int serialNumberFromTheSun, int equatorLength, string? errorMessage) GetPlanet(string planetName) 
+        {
+            _serialNumberFromTheSun = 0;
+            _equatorLength = 0;
+            _errorMessage = string.Empty;
+
+            if (_counter >= 3)
+            {
+                Console.WriteLine("You ask too often!");
+                Console.WriteLine();
+            }
+
+            List<string> avaliablePlanet = _planets.Select(r => r.Name).ToList();
+
+            if (avaliablePlanet.Contains(planetName))
+            {
+                _serialNumberFromTheSun = _planets.Where(r => r.Name == planetName).Select(r => r.SerialNumberFromTheSun).FirstOrDefault();
+                _equatorLength = _planets.Where(r => r.Name == planetName).Select(r => r.EquatorLength).FirstOrDefault();
+            }
+            else 
+            { 
+                _errorMessage = "Failed to find planet!"; 
+            }
+            _counter++;
+            return (_serialNumberFromTheSun, _equatorLength, _errorMessage);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var planets = new PlanetCatalog(new("Venus", 2, 38025), new("Earth", 3, 40075), new("Mars", 4, 21344));
+            var inputs = new List<string>() { "Earth", "Lemonia", "Mars" };
+
+            foreach (var input in inputs)
+            {
+                var planet = planets.GetPlanet(input);
+                if (planet.errorMessage == string.Empty)
+                {
+                    Console.WriteLine($"Name: {input}, SerialNumberFromTheSun: {planet.serialNumberFromTheSun}, EquatorLength: {planet.equatorLength}");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine(planet.errorMessage);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
